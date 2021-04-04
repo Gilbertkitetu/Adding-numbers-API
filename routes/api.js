ad85027ba3d263447affe0225e160990222e8a1d
Gilbert kitetu <gilbertkitetu2@gmail.com>
2021-04-04 13:49:13 +0300

    Complete: adding numbers and sending back a response of the results

--- routes/api.js
+++ routes/api.js
@@ -7,8 +7,13 @@ const router = express.Router();
 //get request and add the numbers
 router.get('/addition', function(req, res, next) {
     var num = 2 + 4;
-    var additionTwo = req.query.a + req.query.b;
-    res.send("Addition of two numbers" + num + additionTwo);
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    var additionTwo = a + b;
+    res.send("The Addition of two numbers " + a + " and " + b + " is " + additionTwo);
+
+    // var a = parseFloat(req.query);
+    // res.send(a);
 });
 
 
